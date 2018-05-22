f02ed228cea6801acbfd4579215f9e94d0d2033b
ishiguro <yu_ishiguro@nuco.co.jp>
2018-05-22 12:10:41 +0900

    fix

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 BUCKET_NAME = "dl-vgg-pattern"
 RECIEVE_DIR = "./util/aws_transferer/data/"
-OUTPUT_DIR = "/output/"
+OUTPUT_DIR = "output/"
 MODELS = "models/"
 THUMBNAILS = "thumbnails/"
 NORMAL = "normal/"
